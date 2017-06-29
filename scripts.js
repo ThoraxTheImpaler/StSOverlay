99b97b66a53f1626c13a6d4b38d79f4b60646e77
ThoraxTheImpaler <lilyodadood@gmail.com>
2017-06-29 00:35:48 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	//Underline current topic
+	$("li[class^='activetoggle']").click(function(){
+		$(this).siblings().removeClass('active')
+		$(this).toggleClass('active');
+	});
+
+	//Scroll through topic list
+	x = 0, numToShow = 1, numToIncrement = 1;
+	var $lis = $('#topics').find('li');
+	$lis.slice(x, x + numToShow).show();
+	$('#NEXT, #PREVIOUS').click(function() {
+		x += 1 + $(this).is('#PREVIOUS') * -2;
+		x = Math.min(Math.max(x, 0), $lis.length - numToShow);
+		$lis.hide(500).slice(x, x + numToShow).show(500);
+	});
+});
